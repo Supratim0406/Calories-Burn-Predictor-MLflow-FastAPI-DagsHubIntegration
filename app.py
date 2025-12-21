e40bde7087160f05dfee7eab08bd7bef2a42d200
Supratim Saha <supratimsmail@gmail.com>
2025-12-21 13:17:20 +0530

    Added project files

---
+++ app.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, Form, Request  # Form - Reads form input values from HTML  # Request - Required by FastAPI when using Jinja templates
+from fastapi.templating import Jinja2Templates  # Allows FastAPI to render HTML pages.
+import pandas as pd
+import joblib
+
+# Fast API -> Backend web framework
+app = FastAPI(title="Calories Burn Prediction")
+
+# Load ML pipeline
+pipeline = joblib.load("best_model.pkl")
+
+# Setup templates folder (Tells FastAPI where your HTML files are)
+templates = Jinja2Templates(directory="templates")
+
+# Home Route - Displays the HTML form
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}              # FastAPI + Jinja requires the request object for rendering templates.
+    )
+
+# Predict Route - Receives values submitted from HTML form
+@app.post("/predict")
+def predict(
+    request: Request,
+    Gender: str = Form(...),
+    Age: float = Form(...),
+    Height: float = Form(...),
+    Weight: float = Form(...),
+    Duration: float = Form(...),
+    Heart_Rate: float = Form(...),
+    Body_Temp: float = Form(...)
+):
+    input_data = pd.DataFrame({      # Convert Form Data into DataFrame
+        "Gender": [Gender],
+        "Age": [Age],
+        "Height": [Height],
+        "Weight": [Weight],
+        "Duration": [Duration],
+        "Heart_Rate": [Heart_Rate],
+        "Body_Temp": [Body_Temp]
+    })
+
+    # Make prediction - returns an array
+    prediction = pipeline.predict(input_data)[0]
+
+    # Return Result to HTML Page
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "Calories": round(prediction, 2)
+        }
+    )
